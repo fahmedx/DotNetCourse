37822ecec28346d545025ea574d8b17e3caf45ca
fahmedx <fahmecanaldx@gmail.com>
2022-04-19 23:16:34 -0300

    Started the course

---
+++ 01 - Simple API/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _01___Simple_API.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
